bc82abbcc9630e67bed27651d1754234afc07175
calderonsamuel <samuel.calderon@uarm.pe>
2019-11-16 12:09:34 -0500

    add data and data documentation

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Peruvian Ubigeos.
+#'
+#' A dataset containing three peruvian ubigeos clasification from INEI, RENIEC and SUNAT.
+#'
+#' @format A data frame with 1876 rows and 18 variables
+#' @source \url{https://github.com/CONCYTEC/ubigeo-peru/blob/master/equivalencia-ubigeos-oti-concytec.csv}
+"ubigeos"
