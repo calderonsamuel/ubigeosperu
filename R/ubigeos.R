6c7ce79c6103651e8b83c4f06b3f48e95cfcedf9
calderonsamuel <samuel.calderon@uarm.pe>
2019-11-24 19:58:51 +0000

    link ubigeos and ubigeos_tidy documentation

--- R/ubigeos.R
+++ R/ubigeos.R
@@ -4,4 +4,5 @@
 #'
 #' @format A data frame with 1876 rows and 18 variables
 #' @source \url{https://github.com/CONCYTEC/ubigeo-peru/blob/master/equivalencia-ubigeos-oti-concytec.csv}
+#' @seealso \code{\link{ubigeos_tidy}} for a tidy version of this dataset.
 "ubigeos"
--- R/ubigeos_tidy.R
+++ R/ubigeos_tidy.R
@@ -5,5 +5,5 @@
 #'
 #' @format A data frame with 6206 rows and 4 variables
 #' @source \url{https://github.com/CONCYTEC/ubigeo-peru/blob/master/equivalencia-ubigeos-oti-concytec.csv}
-"ubigeos_tidy"
 #' @seealso \code{\link{ubigeos}} for the original CONCYTEC dataset.
+"ubigeos_tidy"
