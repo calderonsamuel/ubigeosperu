a1fb920785f831db575318af29a77b3af4533dd4
calderonsamuel <samuel.calderon@uarm.pe>
2019-11-24 19:25:39 +0000

    add ubigeos_tidy dataset and import tibble. fix the ubigeos encoding

---
+++ R/ubigeos_tidy.R
@@ -0,0 +1,9 @@
+#' Peruvian Ubigeos (tidy version).
+#'
+#' This is the tidy version of the \code{\link{ubigeos}} dataset. A dataset containing three
+#' peruvian ubigeos clasification from INEI, RENIEC and SUNAT.
+#'
+#' @format A data frame with 6206 rows and 4 variables
+#' @source \url{https://github.com/CONCYTEC/ubigeo-peru/blob/master/equivalencia-ubigeos-oti-concytec.csv}
+"ubigeos_tidy"
+#' @seealso \code{\link{ubigeos}} for the original CONCYTEC dataset.
